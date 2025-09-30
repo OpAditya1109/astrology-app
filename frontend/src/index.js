7c6c6507c00e53cd7262c6d8b6aac85b82ceb8ad
Aditya Yadav <adityay1109@gmail.com>
2025-09-30 20:47:29 +0530

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,20 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// --------------------------
+// Register Service Worker
+// --------------------------
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch((err) => {
+        console.error('Service Worker registration failed:', err);
+      });
+  });
+}
+
 reportWebVitals();
