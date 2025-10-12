9d24e09df50d1ab454c0983bd28c3d0e288e331f
Aditya Yadav <adityay1109@gmail.com>
2025-10-12 11:16:53 +0530

    .

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -183,7 +183,7 @@ export default function Home() {
 
 
 
-      <PanchangCard />
+        {/* <PanchangCard /> */}
 
 {/* ================= Services Section ================= */}
 <section className="relative max-w-7xl mx-auto px-6 py-20 overflow-hidden">
